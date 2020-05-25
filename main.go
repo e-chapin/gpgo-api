b7d70e284f2120e9403c035038618b7b7a6f386c
Eric <ericjamesjameson@gmail.com>
2020-05-25 16:54:03 -0500

    add route to handle markdown

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/russross/blackfriday"
 )
 
 func main() {
@@ -25,5 +26,12 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
+	router.GET("/mark", handleMark)
+
 	router.Run(":" + port)
+}
+
+
+func handleMark( c *gin.Context) {
+	c.String(http.StatusOK, string(blackfriday.Run([]byte("**Hi!**"))))
 }
