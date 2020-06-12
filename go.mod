971c154f08c8c58d9733d045f24a0a83106028ad
Eric <ericjamesjameson@gmail.com>
2020-06-12 12:04:12 -0500

    before moving from pgx to pg

--- go.mod
+++ go.mod
@@ -4,13 +4,12 @@ go 1.12
 
 require (
 	github.com/gin-gonic/gin v0.0.0-20150626140855-4cc2de6207f4
+	github.com/go-pg/pg/v10 v10.0.0-beta.2 // indirect
 	github.com/heroku/x v0.0.0-20171004170240-705849e307dd
+	github.com/jackc/pgx/v4 v4.6.0 // indirect
+	github.com/lib/pq v1.5.2 // indirect
 	github.com/manucorporat/sse v0.0.0-20150604091100-c142f0f1baea // indirect
-	github.com/mattn/go-colorable v0.0.0-20150625154642-40e4aedc8fab // indirect
-	github.com/mattn/go-isatty v0.0.0-20150814002629-7fcbc72f853b // indirect
 	github.com/russross/blackfriday v2.0.0+incompatible
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
-	golang.org/x/net v0.0.0-20150629084131-d9558e5c97f8 // indirect
 	gopkg.in/bluesuncorp/validator.v5 v5.9.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"net/http"
 	"os"
+
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	"github.com/jackc/pgx/v4"
 	"github.com/russross/blackfriday"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+
+
 )
 
 func main() {
@@ -22,14 +31,64 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
+	opt, err := pg.ParseURL(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+
+	db := pg.Connect(opt)
+	defer db.Close()
+
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}
+
+
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
 	router.GET("/mark", handleMark)
+	router.GET("/items", dbHandler(db))
 
 	router.Run(":" + port)
 }
+
+func dbHandler(db pgx.Conn) gin.HandlerFunc {
+	return func(c *gin.Context){
+
+		type Item struct {
+			Id int
+			Text string
+			Description string
+			Url string
+			IsCompleted bool
+		}
+
+		var items []Item
+
+		rows, err := db.Query(context.Background(), "select * from practice_item order by id asc")
+		if err != nil 	{
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Error Reading Practice Items"))
+			return
+		}
+
+		//defer rows.Close()
+		for rows.Next() {
+
+			var i Item
+
+			if err := rows.Scan(&i.Id, &i.Text, &i.Description, &i.Url, &i.IsCompleted); err != nil {
+				c.String(http.StatusInternalServerError,
+					fmt.Sprintf("Error scanning db: ", err))
+			}
+			items = append(items, i)
+		}
+		c.JSON(http.StatusOK, items)
+	}
+}
+
 
 
 func handleMark( c *gin.Context) {
